c0a6a511d9ddd96cebf234792808afbe472f5bcc
WENI HSU <hsu.weni@gmail.com>
2019-09-07 10:49:40 +0800

    7-Finding Elements by CSS and Name

--- mink.php
+++ mink.php
@@ -3,11 +3,14 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use Behat\Mink\Driver\GoutteDriver;
+use Behat\Mink\Driver\Selenium2Driver;
 use Behat\Mink\Session;
 
-$driver = new GoutteDriver();
+//$driver = new GoutteDriver();
+$driver = new Selenium2Driver('chrome');
 
 $session = new Session($driver);
+$session->start();
 
 $session->visit('http://jurassicpark.wikia.com');
 
@@ -17,3 +20,20 @@ var_dump($session->getStatusCode(), $session->getCurrentUrl());
 $page = $session->getPage();
 
 var_dump(substr($page->getText(), 0, 75));
+
+// NodeElement
+$header = $page->find('css', '.WikiaPageContentWrapper .page-header h1');
+var_dump($header->getText());
+
+//$nav = $page->find('css', '.wds-list .wds-is-linked');
+//$linkEl = $nav->find('css', 'li a');
+//var_dump($linkEl->getHtml());
+
+$selectorsHandler = $session->getSelectorsHandler();
+$linkEl = $page->findLink('The Evolution of Claire');
+var_dump($linkEl->getAttribute('href'));
+
+$linkEl->click();
+
+var_dump($session->getCurrentUrl());
+$session->stop();
